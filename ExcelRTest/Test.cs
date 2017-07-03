574eba6be987bf6d8520ec005e8b8a239e9a6d51
Jitender <jitender.kundu@mindfulsas.com>
2017-07-03 15:58:20 +0530

    added test project

---
+++ ExcelRTest/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Channels;
+using ExcelR;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelRTest
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void ExportTester()
+        {
+            var sheet = ExportHelper.GetSheet();
+
+            var data = GetList();
+            sheet.Write(data,ExportHelper.Style.H3,ExportHelper.Color.Aqua);
+            var stream = sheet.Workbook.ToByteArray();
+            sheet.Workbook.Save(@"C:\Users\Cena\Documents\Visual Studio 2015\Projects\ExcelR\ExcelRTest\docs\abc.xlsx");
+        }
+        [TestMethod]
+        public void ImportTester()
+        {
+            var sheet =
+                ImportHelper.GetSheet(
+                    @"C:\Users\Cena\Documents\Visual Studio 2015\Projects\ExcelR\ExcelRTest\docs\abc.xlsx");
+
+           var data= sheet.Read<TestModel>();
+        }
+        private List<TestModel> GetList()
+        {
+            var list = new List<TestModel>
+            {
+                new TestModel {Bool = true, DateTime = DateTime.Now, String = "jitender", Int = 5},
+                new TestModel {Bool = false, String = "jitende4r", Int = 1}
+            };
+            return list;
+        }
+    }
+}
---
+++ ExcelRTest/TestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelR;
+using ExcelR.Attributes;
+
+namespace ExcelRTest
+{
+    public class TestModel
+    {
+        [ExcelRProp(Name = "TestString")]
+        public string String { get; set; }
+
+        public bool Bool { get; set; }
+
+        public DateTime? DateTime { get; set; }
+
+        [ExcelRProp(ColTextColor = "Red")]
+        public int Int { get; set; }
+    }
+
+}
